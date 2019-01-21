1e0913f46a779f7d576ec9430be9292f44225376
macenrola <hugues@1dcore.com>
2019-01-21 16:23:26 +0800

    First Commit

---
+++ xylene_formatting.py
@@ -0,0 +1,10 @@
+def format_mol_for_vasp(rdkitmol):
+	"""
+	:param rdkitmol: Takes a rdkitmol
+	:return: prints a POSCAR file for use in VASP operations
+	"""
+
+	return
+
+if __name__ == "__main__":
+	pass
