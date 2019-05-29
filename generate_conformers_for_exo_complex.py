359b899b2a1a62608e65cfe09da8bec4b6b2a33b
macenrola <hugues@1dcore.com>
2019-05-29 10:27:08 +0800

    generate comformer exo complex

---
+++ generate_conformers_for_exo_complex.py
@@ -0,0 +1,9 @@
+def generate_conformers_for_exo_complex(sdfile, nconf=50):
+	"""
+	PRE: Takes in a sdf file
+	POST: Will write nconfs conformers for that exo complex
+	"""
+	pass
+
+if __name__=="__main__":
+	generate_conformers_for_exo_complex("/home/macenrola/Documents/heptylamine/heptylamine.sdf")
