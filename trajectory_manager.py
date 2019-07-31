3d89f1eccf4c813df31f15619c67f1621e0dc006
macenrola <hugues@1dcore.com>
2019-07-31 09:42:34 +0800

    edited to follow example

--- trajectory_manager.py
+++ trajectory_manager.py
@@ -27,6 +27,5 @@ if __name__ == '__main__':
 	r = pool.map_async(print_trajectory, tasks, callback=results.append)
 	r.wait() # Wait on the results
 	print results
-	r = pool.map_async(launch_trajectory, tasks, callback=results.append)
-	r.wait() # Wait on the results
-	print results
+	r = pool.map(launch_trajectory, tasks)
+	print r
